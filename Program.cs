48e106671045ad0cd977209618b9a3c61a13258b
Avenger2627 <imraninfo97@gmail.com>
2020-12-03 16:38:43 +0530

    [Imran Shaikh] Add: Check option Added

--- Program.cs
+++ Program.cs
@@ -10,7 +10,20 @@ namespace Snake
             int Position = 0;
             Random Dice = new Random();
             int RollDice = Dice.Next(1, 7);
-            Console.WriteLine(RollDice);
+            int Option = Dice.Next(1, 4);
+            //Checking Option
+            switch (Option)
+            {
+                case 1:   
+                    Console.WriteLine("Player Will Stay in Same Block");
+                    break;
+                case 2:
+                    Console.WriteLine("Player Got Ladder...");
+                    break;
+                case 3:
+                    Console.WriteLine("Snake Bit You...");
+                    break;
+            }
         }
     }
 }
