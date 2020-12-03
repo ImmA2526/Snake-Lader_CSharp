12d42b393faf026e5b628fb69d93bfd57e187675
Avenger2627 <imraninfo97@gmail.com>
2020-12-03 16:50:28 +0530

    [Imran Shaikh] Add: Repeat Till Win

--- Program.cs
+++ Program.cs
@@ -8,23 +8,28 @@ namespace Snake
         {
             Console.WriteLine("***********Welcome To Snake And Ladder************");
             int Position = 0;
-            Random Dice = new Random();
-            int RollDice = Dice.Next(1, 7);
-            int Option = Dice.Next(1, 4);
-            //Checking Option
-            switch (Option)
+
+            while (Position != 100)
             {
-                case 1:   
-                    Console.WriteLine("Player Will Stay in Same Block");
-                    break;
-                case 2:
-                    Console.WriteLine("Player Got Ladder...");
-                    break;
-                case 3:
-                    Console.WriteLine("Snake Bit You...");
-                    break;
+                Random Dice = new Random();
+                int Option = Dice.Next(0, 4);
+                int RollDice = Dice.Next(1, 7);
+                Console.WriteLine(RollDice);
+                switch (Option)
+                {
+                    case 1:
+                        Console.WriteLine("Player Will Stay in Same Block");
+                        break;
+                    case 2:
+                        Console.WriteLine("Player Got Ladder...");
+                        Position += RollDice;
+                        break;
+                    case 3:
+                        Console.WriteLine("Snake Bit You...");
+                        Position -= RollDice;
+                        break;
+                }
             }
         }
     }
 }
- 
